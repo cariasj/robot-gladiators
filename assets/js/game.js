80c835195997f69266ab6478fc0fa7edb6678049
charon <cariasjosue@gmail.com>
2022-01-13 11:30:02 -0500

    added var and console log operations

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,17 @@
-window.alert("This is an alert! JavaScript is running!");
+// Prompts user to enter name of robot before fight function is executed and stores that name in a variable called playerName
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Our robot's name is " + playerName);
+
+
+
+// this creates a function named "fight"
+function fight () {
+    window.alert("the fight has begun");
+}
+//fight();
