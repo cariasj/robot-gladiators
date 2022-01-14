688f45a4f99d03e3fa5ddb0617bef5e7180829b4
charon <cariasjosue@gmail.com>
2022-01-14 16:31:30 -0500

    added individual health to enemies

--- assets/js/game.js
+++ assets/js/game.js
@@ -24,10 +24,12 @@ console.log(enemyNames.length);
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function(enemyName) 
-{
+var fight = function(enemyName) {
+    // repeat and execute as long as the enemy-robot is alive 
+  while(enemyHealth > 0) {
+
     // Alert players that they are starting the round
-    window.alert("Welcome to Robot Gladiators!");
+    /* window.alert("Welcome to Robot Gladiators!"); */
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
   
     // if player choses to fight, then fight
@@ -76,7 +78,10 @@ var fight = function(enemyName)
         window.alert("You need to choose a valid option. Try again!");
     }
 }
+  }
 
-for(var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
+  for (var i = 0; i < enemyNames.length; i++) {
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    fight(pickedEnemyName);
   }
